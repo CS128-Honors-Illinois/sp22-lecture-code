75b5cc6b214eb21baf0d657adb8c2626743e9126
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2022-01-25 16:17:42 -0600

    Add initial lecture2 code

---
+++ lecture2/src/bin/int.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x: u8 = 0xFF;
+    let y = x as u64;
+
+    println!("x: {}\ty: {}", x, y);
+}
---
+++ lecture2/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+  let cmd: &str = "cargo run --bin <filename without .rs>";
+
+  println!("Check out ./lecture2/src/bin/ for more code examples!");
+  println!("You can run any file with a main() function using: `{}`", cmd);
+
+  println!("Ex: run the int.rs file using `cargo run --bin int`");
+}
